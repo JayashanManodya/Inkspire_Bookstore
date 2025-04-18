c8c0623c7ffac3a1ec8a2b37b487b11f254c14c7
IT24102217 <komaldissanayaka12345@gmail.com>
2025-04-18 13:27:46 +0530

    add attributes in Book class

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -1,4 +1,61 @@
 package model;
 
 public class Book {
+
+    private int id;
+    private String title;
+    private String author;
+    private double price;
+    private double rating;
+
+    public Book() {
+    }
+
+    public Book(int id, String title, String author, double price, double rating) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
