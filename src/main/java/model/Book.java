02948ab6901840c3f7084fac2639f3e6830a6102
IT24102217 <komaldissanayaka12345@gmail.com>
2025-04-19 18:04:14 +0530

    updated
    update some attributes and methods

--- src/main/java/model/Book.java
+++ src/main/java/model/Book.java
@@ -7,16 +7,22 @@ public class Book {
     private String author;
     private double price;
     private double rating;
+    private String photo;
+    private String description;
+    private String isbn;
 
     public Book() {
     }
 
-    public Book(int id, String title, String author, double price, double rating) {
+    public Book(int id, String title, String author, double price, double rating, String photo, int stockQuantity, String description, String isbn) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.price = price;
         this.rating = rating;
+        this.photo = photo;
+        this.description = description;
+        this.isbn = isbn;
     }
 
     public int getId() {
@@ -58,4 +64,34 @@ public class Book {
     public void setRating(double rating) {
         this.rating = rating;
     }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", rating=" + rating +
+                ", photo='" + photo + '\'' +
+                ", description='" + description + '\'' +
+                ", isbn='" + isbn + '\'' +
+                '}';
+    }
 }
+
