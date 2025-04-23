dc2bdb6f424410a7aad4623fa4cd9a3bdd4fe726
IT24102217 <komaldissanayaka12345@gmail.com>
2025-04-24 05:09:46 +0530

    Update BookService.java

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,4 +1,20 @@
 package service;
 
+import jakarta.servlet.ServletContext;
+import model.Book;
+
+import java.io.*;
+
 public class BookService {
+
+    private final String BOOKS_FILE;
+    private final ServletContext context;
+
+    public BookService(ServletContext context) {
+        this.context = context;
+
+        String realPath = context.getRealPath("/");
+        BOOKS_FILE = realPath + "data" + File.separator + "books.txt";
+        System.out.println("Books file path: " + BOOKS_FILE); // Debug log
+    }
 }
