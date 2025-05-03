f663309bdf9e580266f580988c5ddfd9c87fbe6b
Jayashan Manodya <kpjmp28@gmail.com>
2025-05-03 10:14:06 +0530

    Add missing code line in cart and order controllers (user import line)

--- src/main/java/controller/OrderServlet.java
+++ src/main/java/controller/OrderServlet.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import model.Order;
+import model.User;
 import model.Order.OrderBook;
 import service.BookService;
 import service.CartService;
